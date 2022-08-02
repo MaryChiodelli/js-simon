f894ffb381a2877faa8a07daba4f4aa6e672fa16
MaryChiodelli <marychiodelli97@gmail.com>
2022-08-02 15:28:24 +0200

    display numbers and store user input

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log('hello');
+const numbers = [];
+const userGuess = [];
+while (numbers.length < 5) {
+    const number = getRandomNumber();
+    if (!numbers.includes(number)) {
+        numbers.push(number);
+    }
+}
+
+alert(numbers.join(', '));
+
+setTimeout(() => {
+    for (let i = 0; i < 5; i++) {
+        const number = parseInt(prompt('Inserisci un numero.'));
+        userGuess.push(number);
+    }
+    console.log(userGuess);
+}, 10000);
+
+console.log(numbers);
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
